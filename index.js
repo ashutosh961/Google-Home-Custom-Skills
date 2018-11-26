fc38cc6cdf85ee884a0c7d3e5b68228409ecf501
Ashutosh Upadhye <ashutosh.upadhye96@gmail.com>
2018-11-26 04:05:26 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ restService.post("/echo", function(req, res) {
       mac:mac,
     source: "webhook-echo-sample",
       flag:wake_flag,
-      result:req.body.result.toString(),
+      parame:req.body.result.parameters.echoText.toString(),
      // db:db,
      // filepointer:fs,
   });
